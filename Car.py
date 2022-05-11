088d3481ed5f67c057e76d476d82f5e5ae060fde
axel.auza <axel.auza@funiber.org>
2022-05-11 14:08:35 -0500

    Setter method and fix str method

--- Car.py
+++ Car.py
@@ -1,12 +1,15 @@
 class Car:
     def __init__(self,plateNumber):
         self.plateNumber=plateNumber
+        self.drive=True
     def __str__(self):
         if self.drive:
-            return "Car with plate "+self.plateNumber+" able to drive"
+            return "Car with plate "+str(self.plateNumber)+" able to drive"
         else:
-            return "Car with plate "+self.plateNumber+" not able to drive"
+            return "Car with plate "+str(self.plateNumber)+" not able to drive"
     def disableDrive(self):
         self.drive=False
     def enableDrive(self):
         self.drive=True
+    def getPlatenumber(self):
+        return self.plateNumber
