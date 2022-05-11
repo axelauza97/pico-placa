ddd5bf16beffb6d9d3b6c7cf59f3bdc94e8e5f85
axel.auza <axel.auza@funiber.org>
2022-05-11 12:50:31 -0500

    Implementing abstracts methods and str methods for prints

--- Car.py
+++ Car.py
@@ -1,8 +1,11 @@
 class Car:
     def __init__(self,plateNumber):
         self.plateNumber=plateNumber
-    def __str__(self) -> str:
-        return self.plateNumber
+    def __str__(self):
+        if self.drive:
+            return "Car with plate "+self.plateNumber+" able to drive"
+        else:
+            return "Car with plate "+self.plateNumber+" not able to drive"
     def disableDrive(self):
         self.drive=False
     def enableDrive(self):
--- Input.py
+++ Input.py
@@ -9,22 +9,39 @@ class Input(ABC):
     @abstractmethod
     def check(self):
         pass
+    def getValue(self):
+        return self.value
 
 class Plate(Input):
     def retrieve(self):
         self.value=input("Input plate:\t")    
     def check(self):
-        pass
+        if(not self.value.isdigit()):
+            raise Exception("Error-> plate must be number")
+        if not (len(self.value) ==4 or len(self.value) ==3):
+            raise Exception("Error-> plate must have 3 to 4 number")
     
 class Date(Input):
     def retrieve(self):
         self.value=input("Input date:\t")
 
     def check(self):
-        pass
+        size=len(self.value.split("/"))
+        if(size!=3):
+            raise Exception("Error-> plate must be sliced with '/' ")
+        day, month, year = self.value.split('/')
+        try:
+            self.date=datetime.datetime(int(year), int(month), int(day))
+        except ValueError:
+            raise Exception("Error-> Wrong date format ")
 
 class Time(Input):
     def retrieve(self):
         self.value=input("Input time:\t")
     def check(self):
-        pass
+        size=len(self.value.split(":"))
+        if(size!=2):
+            raise Exception("Error-> time must be sliced with ':' ")
+        ocurrences=re.match(r'([01]?[0-9]|2[0-3]):[0-5][0-9]',self.value)
+        if(ocurrences==None):
+            raise Exception("Error-> Wrong time format ")
